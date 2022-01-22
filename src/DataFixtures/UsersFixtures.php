ca916d3ed282c92c9a46565f57581da2494f44e8
elgato <amzfba.1bestbuy@gmail.com>
2022-01-22 12:50:51 +0100

    DataFixtures/UsersFixtures:

--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -9,7 +9,6 @@ use Faker;
 
 class UsersFixtures extends Fixture
 {
-
     // get ref client
     // foreach client for random(5,12) users
     public function load(ObjectManager $manager): void
@@ -17,11 +16,11 @@ class UsersFixtures extends Fixture
          $faker = Faker\Factory::create('fr_FR');
 
          //recuperer un [] des clients inscrit. la totalite des reference 
-         $clients = [1,2,3,4];
+         $clients = [1,2,3];
          
          //pour chaque client [4] on cree entre 5 et 12 utilisateurs. foreign key  >>>  client_id dans user
          foreach ($clients as $value) {
-             $idClient = $this->getReference('client_'.$clients);
+             $idClient = $this->getReference('client_'.$value); // fait ref a l'Obj
              
             for ($nbUser=0; $nbUser < random_int(3,12); $nbUser++) { 
                 $user = (new User())
@@ -31,7 +30,6 @@ class UsersFixtures extends Fixture
                 $manager->persist($user);
             }
          }
-         
     }
 
     public function getDependencies()
