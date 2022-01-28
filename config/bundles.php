2da35e1323f2fea5b955728ce2e60402aceb81c7
elgato <amzfba.1bestbuy@gmail.com>
2022-01-28 20:08:26 +0100

    riendsofsymfony/rest-bundle installed

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
